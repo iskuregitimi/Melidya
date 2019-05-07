755591c63730c5dd8f2c9bbd0722b5a91ef86047
Gülbahar İnan <glbaharinan.47@gmail.com>
2019-05-07 12:35:02 +0300

    ikinci checkin

---
+++ Melidya/Melidya.BLL/OrderBLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melidya.BLL
+{
+    public class OrderBLL
+    {
+        //RepositoryPattern
+        //public List<Orders> GetOrders(string CustomerId)
+        //{
+        //    return db.Orders.Where(x => x.CustomerID == CustomerId).ToList();
+        //}
+
+        //public List<Order_Details> GetDetails(int OrderId)
+        //{
+        //    return db.Order_Details.Where(x => x.OrderID == OrderId).ToList();
+        //}
+
+        //public Orders GetOrder(int OrderId)
+        //{
+        //    return db.Orders.FirstOrDefault(x => x.OrderID == OrderId);
+        //}
+
+    }  
+}
