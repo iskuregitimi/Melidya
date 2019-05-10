c9f9c088b8763b40fc164c0d40c7e6a5eb393e09
tolga.kamci <tolga.kamci@BK-LAB204-IS020>
2019-05-10 11:42:54 +0300

    .

---
+++ Melidya/Melidya.BLL/EmployeeBLL.cs
@@ -0,0 +1,24 @@
+﻿using Melidya.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melidya.BLL
+{
+    public static class EmployeeBLL
+    {
+        static NorthwindModel context = new NorthwindModel();
+        public static Employee getEmployee(object username, object password)
+        {
+            return context.Employees.FirstOrDefault(x => x.FirstName == username.ToString() && x.Password == password.ToString());
+        }
+
+        public static void updateEmployee(Employee emp)
+        {
+            context.SaveChanges();
+        }
+    }
+}
+
---
+++ Melidya/Melidya.WebUI.Admin/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using Melidya.BLL;
+using Melidya.DAL;
+using Melidya.WebUI.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Melidya.WebUI.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login(LoginModel model)
+        {
+            Employee employee = EmployeeBLL.getEmployee(model.username, model.password);
+            if (employee != null)
+            {
+                Session["Login"] = employee;
+            }
+
+            return View();
+        }
+
+        public ActionResult profile(LoginModel model)
+        {
+            Customer customer = Session["Login"] as Customer;
+            return View(customer);
+        }
+
+        public ActionResult update(Customer cust)
+        {
+            Customer customer = Session["Login"] as Customer;
+            customer.CustomerID = cust.CustomerID;
+            customer.Password = cust.Password;
+            customer.CompanyName = cust.CompanyName;
+            customer.ContactName = cust.ContactName;
+            customer.ContactTitle = cust.ContactTitle;
+            customer.Address = cust.Address;
+            customer.City = cust.City;
+            customer.Region = cust.Region;
+            customer.PostalCode = cust.PostalCode;
+            customer.Country = cust.Country;
+            customer.Phone = cust.Phone;
+            customer.Fax = cust.Fax;
+            CustomerBLL.updateCustomer(customer);
+            return RedirectToAction("profile");
+        }
+
+        public ActionResult logout()
+        {
+            Session["Login"] = null;
+            return RedirectToAction("Index");
+        }
+    }
+}
