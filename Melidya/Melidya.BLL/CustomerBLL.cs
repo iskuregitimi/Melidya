3c08b89878de112454b5ad4933807cdf531bbcd9
mesut.alkan <mesut.alkan@BK-LAB204-IS004>
2019-05-10 11:09:48 +0300

    Müşteriler listeleniyo sadece

---
+++ Melidya/Melidya.BLL/CustomerBLL.cs
@@ -0,0 +1,19 @@
+﻿using Melidya.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melidya.BLL
+{
+    public class CustomerBLL
+    {
+        DataContext dataContext = new DataContext();
+        
+        public List<Customers> GetCustomers()
+        {
+            return dataContext.Customers.ToList();
+        }
+    }
+}
---
+++ Melidya/Melidya.WebUI/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Melidya.BLL;
+using Melidya.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Melidya.WebUI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        CustomerBLL customerBLL = new CustomerBLL();
+        public ActionResult CustomerList()
+        {
+            List<Customers> customer = customerBLL.GetCustomers();
+
+            return View(customer);
+        }
+    }
+}
