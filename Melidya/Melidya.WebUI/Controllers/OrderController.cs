3fd393ff2c69592b29c1c74accc466ac6d6f9535
mesut.alkan <mesut.alkan@BK-LAB204-IS004>
2019-05-13 14:26:41 +0300

    Order details ekliyodum en son

---
+++ Melidya/Melidya.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Melidya.BLL;
+using Melidya.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Melidya.WebUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        OrderBLL orderBLL = new OrderBLL();
+        public ActionResult Orders()
+        {
+            var user =Session["User"] as Customers;
+            var orderList = orderBLL.GetOrders(user.CustomerID);
+            return View(orderList);
+        }
+
+        public ActionResult OrderDetails()
+        {
+            orderBLL.GetOrderDetails();
+
+            return View();
+        }
+    }
+}
