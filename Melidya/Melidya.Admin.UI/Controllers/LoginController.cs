fd42f477341cb51f6b65508068521720479dfb2b
Goktug Ercaliskan <goktug.ercaliskan@BK-LAB204-IS025>
2019-05-13 15:00:46 +0300

    adminUi eklendi

---
+++ Melidya/Melidya.Admin.UI/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Melidya.Admin.UI.Models;
+using Melidya.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Melidya.Admin.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult LoginHata()
+        {
+
+            return View();
+        }
+        public ActionResult Login(LoginModel loginModel)
+        {
+            CustomerManager cmgr = new CustomerManager();
+            var customer = cmgr.GetCustomer(loginModel.UserName);
+            if (customer.Password == loginModel.Password)
+            {
+                Session.Add("User", customer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("LoginHata");
+            }
+            
+        }
+
+    }
+}
---
+++ Melidya/Melidya.Admin.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Melidya.Admin.UI.MvcApplication" Language="C#" %>
