12bbfe29a88581fc6834fc1282b698abf5cebc8e
fadile avcı <fadile.155@gmail.com>
2019-05-21 10:51:29 +0300

    changes

---
+++ Melidya/Melidya.AdminUI/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Melidya.AdminUI.Models;
+using Melidya.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Melidya.AdminUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+		public ActionResult Login(LoginModel model )
+		{
+			CustomerManager cstmr = new CustomerManager();
+
+			var customer = cstmr.GetCustomer(model.username);
+			if (customer != null && customer.Password == model.password)
+			{
+				Session.Add("User", customer);
+				return RedirectToAction("Index");
+			}
+
+			else{
+				return RedirectToAction("LoginHata");
+			}
+		
+		}
+		public ActionResult LoginHata()
+		{
+			return View();
+		}
+
+
+
+	}
+
+
+}
