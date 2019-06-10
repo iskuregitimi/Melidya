1aec030f01addfcbde275b890de706c7e0e65514
levent.tudor@gmail.com <levent.tudor@gmail.com>
2019-06-10 14:02:37 +0300

    :D

---
+++ Melidya/Melidya.AdminUI/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Melidya.AdminUI.Models;
+using Melidya.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Melidya.AdminUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult LoginHata()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login(LoginModel loginmodel)
+        {
+            CustomerManager cmgr = new CustomerManager();
+            var customer = cmgr.GetCustomer(loginmodel.UserName);
+            if(    customer!=null && customer.Password==loginmodel.Password  )
+            {
+                Session.Add("User", customer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("LoginHata");
+            }
+
+            
+
+        }
+    }
+}
